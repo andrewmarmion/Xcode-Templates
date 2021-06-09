145b5b04bfb5bef7bceb43b86e880fa4f4115cf2
Andrew Marmion <andrew.marmion@gmail.com>
2021-06-09 15:22:30 +0100

    fix: QualityCoding crash

--- Swift XCTest Test Suite (qualitycoding.org).xctemplate/___FILEBASENAME___.swift
+++ Swift XCTest Test Suite (qualitycoding.org).xctemplate/___FILEBASENAME___.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
 
-    func test_zero() {
+    func test_zero() throws {
         XCTFail("Tests not yet implemented in ___FILEBASENAMEASIDENTIFIER___")
     }
 }
